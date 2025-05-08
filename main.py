5760455400e83e576b7a30a6a431ad86cb13cd72
Hyper Pulsor <rakanfasya@gmail.com>
2025-05-08 13:44:37 +0700

    Feat: Added check for catpive portal for Task Scheduler

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import sys
 import os
 import json
-from PyQt5.QtWidgets import QApplication
+import requests
+from PyQt5.QtWidgets import QApplication, QMessageBox
+from PyQt5.QtCore import QTimer
 from main_window import MainWindow
 
 def ensure_known_web_storage():
@@ -11,8 +13,31 @@ def ensure_known_web_storage():
         with open(known_web_path, 'w') as f:
             json.dump({}, f)
 
+def is_captive_portal():
+    url = "http://www.msftconnecttest.com/connecttest.txt"
+    url_legacy = "http://www.msftncsi.com/ncsi.txt"
+    expected_resp = "Microsoft Connect Test"
+    expected_resp_legacy = "Microsoft NCSI"
+    try:
+        response = requests.get(url, allow_redirects=True, timeout=5)
+        response_legacy = requests.get(url_legacy, allow_redirects=True, timeout=5)
+        text_response = response.text
+        text_response_legacy = response_legacy.text
+        if text_response != expected_resp and text_response_legacy != expected_resp_legacy:
+            return True
+        else:
+            return False
+    except requests.RequestException as e:
+        print("Error:", e)
+        return False
+
+
 def main():
     ensure_known_web_storage()
+    if not is_captive_portal():
+        app = QApplication(sys.argv)
+        QMessageBox.critical(None, "No Captive Portal", "No captive portal detected. Exiting...")
+        sys.exit(0)
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
